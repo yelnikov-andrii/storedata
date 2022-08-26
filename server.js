964c883ae7b6cf939b030b39bb6b41692bed9296
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-08-26 12:28:11 +0300

    fixed an error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const ruter = jsonServer.router('mockend.json');
+const router = jsonServer.router('mockend.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
